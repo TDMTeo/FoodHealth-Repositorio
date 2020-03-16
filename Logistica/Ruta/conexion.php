a64ca98c272eea1f398c91271a7b39533a2d6462
TeoPort <nautilius92@outlook.com>
2020-03-16 10:42:12 -0500

    Cambio para ocacionar error

--- Logistica/Ruta/conexion.php
+++ Logistica/Ruta/conexion.php
@@ -1,10 +1,10 @@
 <?php
-
+// se hizo este cambio para verificar el probable problema que vamos a tener con los puertos de victor
 $contraseña = "";
 $usuario = "root";
 $nombre_base_de_datos = "foodhealth";
 try{
-	$base_de_datos = new PDO('mysql:host=localhost:3306;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+	$base_de_datos = new PDO('mysql:host=localhost:3307;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 	 $base_de_datos->query("set names utf8;");
     $base_de_datos->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
