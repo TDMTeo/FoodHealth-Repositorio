2965951b46f31fc409703739e06a3be6c91bfabc
MateoArias <nautilius92@gmail.com>
2020-03-19 12:28:25 -0500

    Cambie el titulo de la ruta

--- Logistica/Ruta/index.php
+++ Logistica/Ruta/index.php
@@ -17,7 +17,7 @@ $Ruta = $sentencia->fetchAll(PDO::FETCH_OBJ);
 <html lang="en">
 
 <head>
-  <title>Rutas</title>
+  <title>Rutas lorem  </title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport" />
